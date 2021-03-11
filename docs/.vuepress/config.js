1fb14ff23d287c1fd1fc563f7660f22c80ad2937
CrabSAMA <71915282@qq.com>
2021-03-11 16:17:05 +0800

    初始化 travis.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/public/favicon.ico' }]
   ],
-  base: '/',
+  base: '/CrabSAMA-Blog/',
   theme: 'reco',
   themeConfig: {
     logo: '/assets/img/logoa.png',
